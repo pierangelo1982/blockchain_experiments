b5dbd9a5d372e56ad2185ef4b97d9822b12f4ec3
pierangelo1982 <pierangelo1982@gmail.com>
2021-04-02 20:19:59 +0200

    basic contract

--- README.md
+++ README.md
@@ -5,3 +5,35 @@ npm install -g truffle
 truffle init
 
 create a package.json file
+
+
+truffle compile
+
+crea migrazione copia file come da tutorial
+
+truffle migrate
+
+verifica
+truffle console
+
+
+```
+todoList = await TodoList.deployed()
+
+truffle(development)> todoList.address
+'0x19fec0e440d78261F1426d189B8EF56CF514b66F'
+
+truffle(development)> todoList.taskCount()
+BN { negative: 0, words: [ 0, <1 empty item> ], length: 1, red: null }
+truffle(development)> taskCount = await todoList.taskCount()
+undefined
+truffle(development)> taskCount
+BN { negative: 0, words: [ 0, <1 empty item> ], length: 1, red: null }
+truffle(development)> taskCount.toNumber
+[Function: toNumber]
+truffle(development)> taskCount.toNumber()
+0
+truffle(development)> 
+
+```
+
--- eth-todo-list/truffle-config.js
+++ eth-todo-list/truffle-config.js
@@ -47,6 +47,13 @@ module.exports = {
     //  port: 8545,            // Standard Ethereum port (default: none)
     //  network_id: "*",       // Any network (default: none)
     // },
+
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
@@ -85,10 +92,10 @@ module.exports = {
       // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
+      optimizer: {
+      enabled: true,
+      runs: 200
+    },
       //  evmVersion: "byzantium"
       // }
     }
