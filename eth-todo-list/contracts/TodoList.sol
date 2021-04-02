c355300ab1c4f65306f726e71de7aabc0fcc6888
pierangelo1982 <pierangelo1982@gmail.com>
2021-04-02 21:07:38 +0200

    retieve data

--- eth-todo-list/contracts/TodoList.sol
+++ eth-todo-list/contracts/TodoList.sol
@@ -2,7 +2,27 @@ pragma solidity ^0.5.16;
 
 contract TodoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Prova a fare una blockchain");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
 }
+
+
 
 
 
